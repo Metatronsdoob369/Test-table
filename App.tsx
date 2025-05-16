fea1207aee4ccfa173ed14d7b933dc417627f9a5
MetatronScoob_369 <phooten4@gmail.com>
2025-05-15 20:08:48 -0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,1 +1,10 @@
-// BookaPhi main app component
+// BookaPhi main app componentimport React from 'react';
+
+export default function App() {
+  return (
+    <div>
+      <h1>BookaPHI: Online</h1>
+      <p>This is the beginning of everything...</p>
+    </div>
+  );
+}
