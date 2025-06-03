3429fe97dc871c5715cf638c8a15e8ce4416300d
MetatronScoob_369 <phooten4@gmail.com>
2025-06-03 16:48:34 -0500

    Fix App import path in main.tsx

--- main.tsx
+++ main.tsx
@@ -1,6 +1,6 @@
-// BookaPhi app entry pointimport React from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './src/App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
