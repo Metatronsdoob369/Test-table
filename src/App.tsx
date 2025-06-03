c4eae1abda3854342770262651655f1976d2149c
MetatronScoob_369 <phooten4@gmail.com>
2025-06-03 16:33:46 -0500

    Fix React import statements

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+// BookaPhi main app component
+
+export default function App() {
+  return (
+    <div>
+      <h1>BookaPHI: Online</h1>
+      <p>This is the beginning of everything...</p>
+    </div>
+  );
+}
