f02233ecca05aa218a8b8a7bdb034f88896ed130
MetatronScoob_369 <phooten4@gmail.com>
2025-06-03 16:44:43 -0500

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-// BookaPhi main app component
 
+// BookaPHI main app component
 export default function App() {
   return (
-    <div>
-      <h1>BookaPHI: Online</h1>
+    <div style={{ fontFamily: "sans-serif", padding: "2rem" }}>
+      <h1>📘 BookaPHI: Online</h1>
       <p>This is the beginning of everything...</p>
     </div>
   );
